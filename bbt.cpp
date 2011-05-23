09fc8e35791dbf1245a73bcfb828d7bba6d297c9
buryhuang <buryhuang@2e8425c3-5cc5-42cb-a704-b9eb6d79ceed>
2011-05-23 23:43:37 +0000

    Main window added.

--- bbt.cpp
+++ bbt.cpp
@@ -2,6 +2,12 @@
 //
 //#include "stdafx.h"
 #include "InstanceMonitor.h"
+#include "Shellapi.h"
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
 
 #ifdef UNITTEST
 #include "gtest/gtest.h"
@@ -21,6 +27,7 @@ using namespace std;
 #endif
 
 
+#define TRAY_NOTIFY (WM_USER + 1)
 
 // 唯一的应用程序对象
 
@@ -38,4 +45,152 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 	theApp.AppStart();
 #endif
 	return 0;
+}
+
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("win32app");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
+
+HINSTANCE hInst;
+NOTIFYICONDATA nid;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+    TCHAR greeting[] = _T("Hello, World!");
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        // Here your application is laid out.
+        // For this introduction, we just print out "Hello, World!"
+        // in the top left corner.
+        TextOut(hdc,
+            5, 5,
+            greeting, _tcslen(greeting));
+        // End application specific layout section.
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+	case TRAY_NOTIFY:
+		switch(lParam)
+		{
+		case WM_LBUTTONDBLCLK:
+			MessageBox(NULL, L"Tray icon double clicked!", L"clicked", MB_OK);
+			break;
+		case WM_RBUTTONUP:
+			if(IDOK == MessageBox(NULL, L"Are you sure to quit Health Monitor?", L"Quit?", MB_OKCANCEL)) {
+				Shell_NotifyIcon(NIM_DELETE, &nid);
+				PostQuitMessage(0);
+				return 0;
+			};
+			break;
+		default:
+			   return DefWindowProc(hWnd, message, wParam, lParam);
+		};
+		break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpszCmd, int nCmdShow)
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+    wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+    {
+        MessageBox(NULL,
+            _T("Call to RegisterClassEx failed!"),
+            _T("Win32 Guided Tour"),
+            NULL);
+
+        return 1;
+    }
+
+	static TCHAR szWindowClass[] = _T("win32app");
+	static TCHAR szTitle[] = _T("Win32 Guided Tour Application");
+
+	// The parameters to CreateWindow explained:
+	// szWindowClass: the name of the application
+	// szTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application does not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd,nCmdShow);
+	UpdateWindow(hWnd);
+
+	nid.cbSize = sizeof(NOTIFYICONDATA);
+	nid.hWnd = hWnd;
+	nid.uID = 100;
+	nid.uVersion = NOTIFYICON_VERSION;
+	nid.uCallbackMessage = TRAY_NOTIFY;
+	nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcscpy_s(nid.szTip, L"Tray Icon");
+	nid.uFlags = NIF_MESSAGE|NIF_ICON|NIF_TIP;
+	 
+	Shell_NotifyIcon(NIM_ADD, &nid);
+
+	MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
 }
